542292cdb146c06cca6d03909e8265f0461f9b9c
sirvenny <joshvenn80@gmail.com>
2018-05-02 13:14:45 +0100

    Read COM1 and output 100 bytes, added docstring

--- serial_sniffer.py
+++ serial_sniffer.py
@@ -1,1 +1,12 @@
 import serial
+import sys
+
+"""Reads serial port COM1 and returns a file with recorded data"""
+
+try:
+  ser = serial.Serial('\\.\COM1', 4800,timeout=None, parity=serial.PARITY_EVEN, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
+
+except:
+    sys.exit("CANNOT CONNECT")
+
+s = ser.read(100)
